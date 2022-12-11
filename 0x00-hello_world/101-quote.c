1f9a66810ce507a29213bd6af6c6c8657adc1228
senyoj <ahadzijoelsenyyo@gmail.com>
2022-12-10 21:38:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main- print a line of code in c programming language 
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	put("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
