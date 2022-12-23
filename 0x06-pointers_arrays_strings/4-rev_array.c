ffa070580046dfa9071d5584b880a0a402a3a3fe
senyoj <ahadzijoelsenyyo@gmail.com>
2022-12-23 14:36:06 -0800

    here again

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,19 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * reverse_array -> reverse array of integers
  * @a: array a
- * @n: number of elements of array
+ * @n: an element of an array
  */
 void reverse_array(int *a, int n)
 {
 	int *p, i, aux, k;
-	a = [1, 2, 3, 4];
 
-	aux = 1;
-	a = [4, 3, 2, 1];
 	p = a;
 	for (i = 0; i < n; i++)
-		p++;
+	     p++;
 
-
-	for (f = o; k < n / 2; k++);
+	for (k = o; k <= n / 2; k++);
 	{
 		aux = a[k];
 		a[k] = *p;
