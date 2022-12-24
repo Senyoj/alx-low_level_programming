1cdd6c3fbf778b68febb8ee43bba266c7edbbc2e
senyoj <ahadzijoelsenyyo@gmail.com>
2022-12-23 21:21:12 -0800

    here again

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_troupper -> converting any string to upper case
+ * @X: string param
+ * Return: string
+ */
+char *string_toupper(char *);
+{
+	int i = 0;
+
+	while (X[i])
+	{
+		if (X[i] >= 97 && x[i] <= 122)
+			X[1] = x[i] - 32;
+		i++
+	}
+	return (X);
+
